43928dd3c82369e61b3debdcc7f636a7f7074ef9
Saendhor <comix143.mariorossi@gmail.com>
2025-03-12 18:08:30 +0100

    feat: modified code in FibonacciTest.java (Singleton pattern); feat: added Design Pattern(s) pdfs

--- JavaCode/Pattern/Creazionali/Singleton/FibonacciTest.java
+++ JavaCode/Pattern/Creazionali/Singleton/FibonacciTest.java
@@ -1,24 +1,45 @@
 public class FibonacciTest {
 
-    public static void TestNumValues(Fibonacci object) {
+    //Checks the total number of item in the values array of the Fibonacci object
+    public static void printTestNumValues(Fibonacci object) {
         System.out.println("Total number of values is: " + object.getNumValues() + "\n");
 
     }
 
-    public static void TestValue(Fibonacci object) {
+    //Here lies the test so that it can be passed to multiple objects hence verifying
+    //if the Singleton pattern has been implemented properly
+    public static void TestInMain(Fibonacci object) {
+        int offset = 6;
+
+        FibonacciTest.printTestNumValues(object);
+
+        object.setIndex(object.getIndex());
+        FibonacciTest.printTestValue(object);
+
+        object.setIndex(5);
+        FibonacciTest.printTestValue(object);
+
+        object.setIndex(object.getNumValues() + offset); // int + int
+        FibonacciTest.printTestValue(object);
+
+    }
+
+    //Checks current index and the value within it
+    public static void printTestValue(Fibonacci object) {
         System.out.println("Current index value = " + object.getIndex());
         System.out.println("The current value is " + object.getValue() + "\n");
 
     }
 
     public static void main(String[] args) {
-        Fibonacci fibObj = Fibonacci.getInstance();
-        FibonacciTest.TestValue(fibObj);
+        System.out.println("First item tested");
+        Fibonacci fibObj1 = Fibonacci.getInstance();
+        FibonacciTest.TestInMain(fibObj1);
+        
+        System.out.println("\n");
 
-        fibObj.setIndex(5);
-        FibonacciTest.TestValue(fibObj);
-
-        fibObj.setIndex(fibObj.getNumValues() +2);
-        FibonacciTest.TestValue(fibObj);
+        System.out.println("Second item tested");
+        Fibonacci fibObj2 = Fibonacci.getInstance();
+        FibonacciTest.TestInMain(fibObj2);
     }
 }

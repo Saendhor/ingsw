be4fd7c64d86cfb0894d99c165889e7d261a8543
Saendhor <comix143.mariorossi@gmail.com>
2025-03-13 14:08:04 +0100

    feat: Corrected Singleton test; feat: implementing Multiton pattern

---
+++ JavaCode/Pattern/Creazionali/Multiton/MultiFibonacci.java
@@ -0,0 +1,48 @@
+public class MultiFibonacci {
+    private int objId = 0;
+    private int index;
+    private int[] values = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233};
+    private static int numMaxInstances = 2;
+    private static int numInstances = 0;
+    private static MultiFibonacci instance;
+
+    //Constructor
+    private MultiFibonacci() {
+        objId = numInstances;
+    }
+
+    public int getNumValues() {
+        return values.length;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getInstanceId() {
+        return objId;
+    }
+
+    public static MultiFibonacci getInstance() {
+        if(numInstances <= numMaxInstances) {
+            System.out.println("Creating a new instance");
+            numInstances++;
+            instance = new MultiFibonacci();
+        }
+        return instance;
+    }
+
+    //Query method
+    public int getValue() {
+        //Make sure index respects values array boundaries
+        if(index < values.length) {
+            index++;
+        }
+        return values[index-1];
+    }
+
+    //Command method
+    public void setIndex(int inputIndex) {
+        index = inputIndex % values.length;
+    }
+}
--- JavaCode/Pattern/Creazionali/Multiton/MultiFibonacciTest.java
+++ JavaCode/Pattern/Creazionali/Multiton/MultiFibonacciTest.java
@@ -1,37 +1,37 @@
-public class FibonacciTest {
+public class MultiFibonacciTest {
 
-    //Checks the total number of item in the values array of the Fibonacci object
-    public static void printTestNumValues(Fibonacci object) {
+    //Checks the total number of item in the values array of the MultiFibonacci object
+    public static void printTestNumValues(MultiFibonacci object) {
         System.out.println("Total number of values is: " + object.getNumValues() + "\n");
 
     }
 
     //Here lies the test so that it can be passed to multiple objects hence verifying
     //if the Singleton pattern has been implemented properly
-    public static void TestInMain(Fibonacci object) {
+    public static void TestInMain(MultiFibonacci object) {
         //Magic numbers determined by the user who's running the test
         int customIndex = 5;
         int offset = 6;
 
         //Visualize total number of item in the array
-        FibonacciTest.printTestNumValues(object);
+        MultiFibonacciTest.printTestNumValues(object);
 
         //Set the index to the last selected index, if default it should be 3 else
         //the same index of the last iteration
         object.setIndex(object.getIndex());
-        FibonacciTest.printTestValue(object);
+        MultiFibonacciTest.printTestValue(object);
 
         //Set custom index selected by the user
         object.setIndex(customIndex);
-        FibonacciTest.printTestValue(object);
+        MultiFibonacciTest.printTestValue(object);
 
         object.setIndex(object.getNumValues() + offset); // int + int
-        FibonacciTest.printTestValue(object);
+        MultiFibonacciTest.printTestValue(object);
 
     }
 
     //Checks current index and the value within it
-    public static void printTestValue(Fibonacci object) {
+    public static void printTestValue(MultiFibonacci object) {
         System.out.println("Current index value = " + object.getIndex());
         System.out.println("The current value is " + object.getValue() + "\n");
 
@@ -39,13 +39,14 @@ public class FibonacciTest {
 
     public static void main(String[] args) {
         System.out.println("First item tested");
-        Fibonacci fibObj1 = Fibonacci.getInstance();
-        FibonacciTest.TestInMain(fibObj1);
+        MultiFibonacci fibObj1 = MultiFibonacci.getInstance();
+        MultiFibonacciTest.TestInMain(fibObj1);
         
         System.out.println("\n");
 
         System.out.println("Second item tested");
-        Fibonacci fibObj2 = Fibonacci.getInstance();
-        FibonacciTest.TestInMain(fibObj2);
+        MultiFibonacci fibObj2 = MultiFibonacci.getInstance();
+        MultiFibonacciTest.TestInMain(fibObj2);
     }
 }
+
